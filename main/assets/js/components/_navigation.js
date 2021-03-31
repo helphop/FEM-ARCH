e0c43be785fb67406048463203c3d9da6c8b8692
Mitchell Gould <mitchellgould7@gmail.com>
2021-03-31 16:58:30 -0400

    complete correct aria-collapsable menu

--- main/assets/js/components/_navigation.js
+++ main/assets/js/components/_navigation.js
@@ -1,6 +1,29 @@
 const navbar = document.querySelector('.nav-bar');
 const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav');
 
-navIcon.addEventListener('click', () => {
+navIcon.addEventListener('click', () => toggleNavbar())
+
+// listen for key events
+window.addEventListener('keyup', function(event){
+  // listen for esc key
+  if( keyPressed(event, 27, 'escape') && navbarOpen()) {
+    toggleNavbar();
+  } else if (keyPressed(event, 36, 'enter') && !navbarOpen()) {
+    toggleNavbar();
+  }
+});
+
+//close or open the navbar and change aria-expanded state
+const toggleNavbar = () => {
   navbar.classList.toggle('open');
-})
+  state = navbarOpen();
+  navIcon.setAttribute('aria-expanded', state);
+  nav.hidden = !nav.hidden;
+};
+
+//check if navbar is open
+const navbarOpen = () => navbar.classList.contains('open') ? true : false;
+
+//check if key has been pressed
+const keyPressed = (event, key, label) => (event.key && event.key == key) || (event.key && event.key.toLowerCase() == label)
