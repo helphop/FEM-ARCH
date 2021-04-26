9f17dab40e6f3fe4f6ed400a8ee7a09d428b7328
Mitchell Gould <mitchellgould7@gmail.com>
2021-04-26 16:00:23 -0400

    correct js to use navigation element

--- main/assets/js/components/_navigation.js
+++ main/assets/js/components/_navigation.js
@@ -1,28 +1,28 @@
-const navbar = document.querySelector('.nav-bar');
+const navigation = document.querySelector('.navigation');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.nav');
 
-navIcon.addEventListener('click', () => toggleNavbar())
+navIcon.addEventListener('click', () => toggleNavigation())
 
 // listen for key events
 window.addEventListener('keyup', function(event){
   // listen for esc key
   if( keyPressed(event, 27, 'escape') && navbarOpen()) {
-    toggleNavbar();
+    toggleNavigation();
   } else if (keyPressed(event, 36, 'enter') && !navbarOpen()) {
-    toggleNavbar();
+    toggleNavigation();
   }
 });
 
-//close or open the navbar and change aria-expanded state
-const toggleNavbar = () => {
-  navbar.classList.toggle('open');
-  navIcon.setAttribute('aria-expanded', navbarOpen());
+//close or open the navigation and change aria-expanded state
+const toggleNavigation = () => {
+  navigation.classList.toggle('open');
+  navIcon.setAttribute('aria-expanded', navigationOpen());
   nav.hidden = !nav.hidden;
 };
 
-//check if navbar is open
-const navbarOpen = () => navbar.classList.contains('open') ? true : false;
+//check if navigation is open
+const navigationOpen = () => navigation.classList.contains('open') ? true : false;
 
 //check if key has been pressed
 const keyPressed = (event, key, label) => (event.key && event.key == key) || (event.key && event.key.toLowerCase() == label)
